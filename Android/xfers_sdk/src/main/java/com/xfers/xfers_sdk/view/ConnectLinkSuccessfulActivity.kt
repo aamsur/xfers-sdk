958d92fc0e14b322167f15ac48ed4fa5acc0e4bb
junhaoyap <lordyyap@gmail.com>
2018-10-18 16:58:49 +0800

    Add Connect Link Successful Activity

---
+++ Android/xfers_sdk/src/main/java/com/xfers/xfers_sdk/view/ConnectLinkSuccessfulActivity.kt
@@ -0,0 +1,27 @@
+package com.xfers.xfers_sdk.view
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import com.xfers.xfers_sdk.R
+import com.xfers.xfers_sdk.utils.XfersConfiguration
+
+class ConnectLinkSuccessfulActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_connect_link_successful)
+
+        val merchantTextView = findViewById<TextView>(R.id.linkSuccessfulMerchantTextView)
+        merchantTextView.text = "Your Xfers account has now been successfully linked to ${XfersConfiguration.getMerchantName()}."
+
+        val returnButton = findViewById<TextView>(R.id.linkSuccessfulReturnButton)
+        returnButton.text = "Return to ${XfersConfiguration.getMerchantName()}"
+    }
+
+    fun onClickReturn(view: View) {
+        // TODO: Pop back to Merchant's context, finish() for now
+        finish()
+    }
+}
