80c22c682cae7dfb5d8fc9292eda63780b189cbb
junhaoyap <lordyyap@gmail.com>
2018-10-18 15:37:12 +0800

    Add merchant name in connect phone page

--- Android/xfers_sdk/src/main/java/com/xfers/xfers_sdk/view/ConnectPhoneActivity.kt
+++ Android/xfers_sdk/src/main/java/com/xfers/xfers_sdk/view/ConnectPhoneActivity.kt
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.xfers.xfers_sdk.R
+import android.widget.TextView
+import com.xfers.xfers_sdk.utils.XfersConfiguration
 
 class ConnectPhoneActivity : AppCompatActivity() {
 
@@ -12,6 +14,9 @@ class ConnectPhoneActivity : AppCompatActivity() {
         setContentView(R.layout.activity_connect_phone)
 
         title = "Link Xfers Account"
+
+        val merchantTextView = findViewById<TextView>(R.id.phoneNumberMerchantTextView)
+        merchantTextView.text = "Connecting to ${XfersConfiguration.getMerchantName()}"
     }
 
     fun onClickBack(view: View) {
