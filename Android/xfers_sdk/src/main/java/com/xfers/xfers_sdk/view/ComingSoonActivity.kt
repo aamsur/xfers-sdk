24c30a385e3db9e9825ba56af110b44cc65a90fe
junhaoyap <lordyyap@gmail.com>
2018-10-22 16:58:54 +0800

    Spruce up coming soon page UI

--- Android/xfers_sdk/src/main/java/com/xfers/xfers_sdk/view/ComingSoonActivity.kt
+++ Android/xfers_sdk/src/main/java/com/xfers/xfers_sdk/view/ComingSoonActivity.kt
@@ -3,6 +3,7 @@ package com.xfers.xfers_sdk.view
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.xfers.xfers_sdk.R
 import com.xfers.xfers_sdk.utils.XfersConfiguration
@@ -16,13 +17,16 @@ class ComingSoonActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_coming_soon)
 
-        title = "COMING SOON"
-
         firebaseAnalytics = FirebaseAnalytics.getInstance(this)
 
         val params = Bundle()
         params.putString("merchant_name", XfersConfiguration.getMerchantName())
         firebaseAnalytics?.logEvent("open_coming_soon", params)
+
+        title = getString(R.string.coming_soon_title)
+
+        val comingSoonBackButton = findViewById<Button>(R.id.comingSoonBackButton)
+        comingSoonBackButton.text = getString(R.string.coming_soon_return_copy, XfersConfiguration.getMerchantName())
     }
 
     fun onButtonClick(view: View) {
