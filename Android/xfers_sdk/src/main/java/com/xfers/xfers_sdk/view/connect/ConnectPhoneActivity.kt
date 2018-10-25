c3141fed7e74d593f504ce0a474e3cff48ce774a
junhaoyap <lordyyap@gmail.com>
2018-10-25 16:44:47 +0800

    Demo usage of reusable xfers_form_input for connect phone number

--- Android/xfers_sdk/src/main/java/com/xfers/xfers_sdk/view/connect/ConnectPhoneActivity.kt
+++ Android/xfers_sdk/src/main/java/com/xfers/xfers_sdk/view/connect/ConnectPhoneActivity.kt
@@ -7,7 +7,6 @@ import android.widget.EditText
 import com.xfers.xfers_sdk.R
 import android.widget.TextView
 import com.xfers.xfers_sdk.task.ConnectPhoneTask
-import com.xfers.xfers_sdk.utils.XfersConfiguration
 
 class ConnectPhoneActivity : AppCompatActivity() {
 
@@ -15,20 +14,26 @@ class ConnectPhoneActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_connect_phone)
 
-        title = "Link Xfers Account"
+        title = getString(R.string.connect_flow_title)
 
-        // TODO: To be removed, for proof of concept only
-        val merchantTextView = findViewById<TextView>(R.id.phoneNumberMerchantTextView)
-        merchantTextView.text = "Connecting to ${XfersConfiguration.getMerchantName()}"
-    }
+        val formInputPageTitle = findViewById<TextView>(R.id.xfersFormInputPageTitle)
+        formInputPageTitle.visibility = View.GONE
 
-    // TODO: To be replaced with Android "Back" and "Up"
-    fun onClickBack(view: View) {
-        finish()
+        val formInputFieldTitle = findViewById<TextView>(R.id.xfersFormInputFieldTitle)
+        formInputFieldTitle.text = getString(R.string.mobile_phone_number_field_title)
+
+        val formInputEditText = findViewById<EditText>(R.id.xfersFormInputEditText)
+        formInputEditText.hint = getString(R.string.phone_number_lorem_ipsum)
+
+        val formInputEditTextSubtitle = findViewById<TextView>(R.id.xfersFormInputEditTextSubtitle)
+        formInputEditTextSubtitle.text = getString(R.string.connect_mobile_phone_number_subtitle)
+
+        val formInputNotesTextView = findViewById<TextView>(R.id.xfersFormInputNotesTextView)
+        formInputNotesTextView.visibility = View.GONE
     }
 
     fun onClickNext(view: View) {
-        val phoneNumberTextField = findViewById<EditText>(R.id.editPhoneNumberTextField)
+        val phoneNumberTextField = findViewById<EditText>(R.id.xfersFormInputEditText)
         val userPhoneNumber = phoneNumberTextField.text.toString()
 
         ConnectPhoneTask(this, userPhoneNumber).execute()
