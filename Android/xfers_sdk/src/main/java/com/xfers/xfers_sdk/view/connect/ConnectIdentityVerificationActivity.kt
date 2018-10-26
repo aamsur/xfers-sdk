9d04f4161528583bbf7e4701cbd4a80f85893984
junhaoyap <lordyyap@gmail.com>
2018-10-26 16:03:09 +0800

    Add reusable xfers_list_view

--- Android/xfers_sdk/src/main/java/com/xfers/xfers_sdk/view/connect/ConnectIdentityVerificationActivity.kt
+++ Android/xfers_sdk/src/main/java/com/xfers/xfers_sdk/view/connect/ConnectIdentityVerificationActivity.kt
@@ -3,10 +3,13 @@ package com.xfers.xfers_sdk.view.connect
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.widget.TextView
 import com.xfers.xfers_sdk.R
 import com.xfers.xfers_sdk.view.shared.ComingSoonActivity
+import com.xfers.xfers_sdk.view.shared.XfersItemRowAdapter
 
 class ConnectIdentityVerificationActivity : AppCompatActivity() {
 
@@ -14,15 +17,18 @@ class ConnectIdentityVerificationActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_connect_identity_verification)
 
+        val icons = arrayListOf(R.drawable.status_success_50, R.drawable.status_success_50)
+        val copies = arrayListOf("Top-ups & Withdrawals", "An increased holding limit of XXX.XXX")
+
+        val listViewRecyclerView = findViewById<RecyclerView>(R.id.listViewRecyclerView)
+        listViewRecyclerView.layoutManager = LinearLayoutManager(this)
+        val adapter = XfersItemRowAdapter(icons, copies)
+        listViewRecyclerView.adapter = adapter
+
         title = "Identity Verification"
 
         val extendedTopbarTextView = findViewById<TextView>(R.id.extendedTopbarTextView)
         extendedTopbarTextView.text = getString(R.string.connect_identity_verification_topbar_copy)
-    }
-
-    // TODO: To be removed, for development purposes only
-    fun onClickBack(view: View) {
-        finish()
     }
 
     fun onClickReject(view: View) {
---
+++ Android/xfers_sdk/src/main/java/com/xfers/xfers_sdk/view/shared/XfersItemRowAdapter.kt
@@ -0,0 +1,30 @@
+package com.xfers.xfers_sdk.view.shared
+
+import android.support.v7.widget.RecyclerView
+import android.view.ViewGroup
+import android.widget.TextView
+import android.view.View
+import android.widget.ImageView
+import com.xfers.xfers_sdk.R
+import android.view.LayoutInflater
+
+class XfersItemRowAdapter(private val icons: ArrayList<Int>, private val copies: ArrayList<String>) : RecyclerView.Adapter<XfersItemRowAdapter.ViewHolder>() {
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        var xfersItemRowImageView: ImageView? = view.findViewById(R.id.xfersItemRowImageView)
+        var xfersItemRowTextView: TextView? = view.findViewById(R.id.xfersItemRowTextView)
+    }
+
+    override fun getItemCount(): Int {
+        return icons.size
+    }
+
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+        viewHolder.xfersItemRowImageView?.setImageResource(icons[position])
+        viewHolder.xfersItemRowTextView?.text = copies[position]
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.xfers_item_row, parent, false))
+    }
+}
