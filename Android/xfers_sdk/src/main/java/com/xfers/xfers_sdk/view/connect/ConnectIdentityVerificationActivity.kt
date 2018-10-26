fe886012bcdcb367d30ea55bd3206dd30f4fddc4
junhaoyap <lordyyap@gmail.com>
2018-10-26 18:32:55 +0800

    Remove another unnecessary import

--- Android/xfers_sdk/src/main/java/com/xfers/xfers_sdk/view/connect/ConnectIdentityVerificationActivity.kt
+++ Android/xfers_sdk/src/main/java/com/xfers/xfers_sdk/view/connect/ConnectIdentityVerificationActivity.kt
@@ -3,7 +3,6 @@ package com.xfers.xfers_sdk.view.connect
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v4.content.ContextCompat
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.widget.Button
