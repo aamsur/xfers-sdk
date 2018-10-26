ea400cdde7a04deaa0f04c264e12338747b4f061
junhaoyap <lordyyap@gmail.com>
2018-10-26 16:51:18 +0800

    Sample build of connect identity verification with includes only

--- Android/xfers_sdk/src/main/java/com/xfers/xfers_sdk/view/connect/ConnectIdentityVerificationActivity.kt
+++ Android/xfers_sdk/src/main/java/com/xfers/xfers_sdk/view/connect/ConnectIdentityVerificationActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import com.xfers.xfers_sdk.R
 import com.xfers.xfers_sdk.view.shared.ComingSoonActivity
@@ -17,25 +18,38 @@ class ConnectIdentityVerificationActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_connect_identity_verification)
 
-        val icons = arrayListOf(R.drawable.status_success_50, R.drawable.status_success_50)
-        val copies = arrayListOf("Top-ups & Withdrawals", "An increased holding limit of XXX.XXX")
+        title = getString(R.string.connect_identity_verification_title)
+
+        val extendedTopbarTextView = findViewById<TextView>(R.id.extendedTopbarTextView)
+        extendedTopbarTextView.text = getString(R.string.connect_identity_verification_topbar_copy)
+
+        val xfersSummaryTitleTextView = findViewById<TextView>(R.id.xfersSummaryTitleTextView)
+        xfersSummaryTitleTextView.text = getString(R.string.connect_identity_verification_summary_title)
+
+        val icons = arrayListOf(
+                R.drawable.status_success_50,
+                R.drawable.status_success_50
+        )
+        val copies = arrayListOf(
+                getString(R.string.connect_identity_verification_listview_topup_withdrawal_copy),
+                getString(R.string.connect_identity_verification_listview_increased_holding_copy)
+        )
 
         val listViewRecyclerView = findViewById<RecyclerView>(R.id.listViewRecyclerView)
         listViewRecyclerView.layoutManager = LinearLayoutManager(this)
         val adapter = XfersItemRowAdapter(icons, copies)
         listViewRecyclerView.adapter = adapter
 
-        title = "Identity Verification"
+        val xfersDoubleButtonsNegativeButton = findViewById<Button>(R.id.xfersDoubleButtonsNegativeButton)
+        xfersDoubleButtonsNegativeButton.text = getString(R.string.later_button_copy)
+        xfersDoubleButtonsNegativeButton.setOnClickListener {
+            startActivity(Intent(this, ConnectLinkSuccessfulActivity::class.java))
+        }
 
-        val extendedTopbarTextView = findViewById<TextView>(R.id.extendedTopbarTextView)
-        extendedTopbarTextView.text = getString(R.string.connect_identity_verification_topbar_copy)
-    }
-
-    fun onClickReject(view: View) {
-        startActivity(Intent(this, ConnectLinkSuccessfulActivity::class.java))
-    }
-
-    fun onClickProceed(view: View) {
-        startActivity(Intent(this, ComingSoonActivity::class.java))
+        val xfersDoubleButtonsPositiveButton = findViewById<Button>(R.id.xfersDoubleButtonsPositiveButton)
+        xfersDoubleButtonsPositiveButton.text = getString(R.string.proceed_button_copy)
+        xfersDoubleButtonsPositiveButton.setOnClickListener {
+            startActivity(Intent(this, ComingSoonActivity::class.java))
+        }
     }
 }
