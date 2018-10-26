78af38e99ed7fcbb5c32c05eb4451bc0d906354b
junhaoyap <lordyyap@gmail.com>
2018-10-26 17:23:52 +0800

    Refactor code style

--- Android/xfers_sdk/src/main/java/com/xfers/xfers_sdk/view/connect/ConnectIdentityVerificationActivity.kt
+++ Android/xfers_sdk/src/main/java/com/xfers/xfers_sdk/view/connect/ConnectIdentityVerificationActivity.kt
@@ -5,7 +5,6 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
-import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import com.xfers.xfers_sdk.R
--- Android/xfers_sdk/src/main/java/com/xfers/xfers_sdk/view/shared/ComingSoonActivity.kt
+++ Android/xfers_sdk/src/main/java/com/xfers/xfers_sdk/view/shared/ComingSoonActivity.kt
@@ -2,7 +2,6 @@ package com.xfers.xfers_sdk.view.shared
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.xfers.xfers_sdk.R
