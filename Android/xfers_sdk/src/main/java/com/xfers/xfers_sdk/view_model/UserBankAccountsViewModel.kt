3c479bc20526a7cc3195250b4ae4a3e44890d552
junhaoyap <lordyyap@gmail.com>
2018-10-31 18:02:00 +0800

    First sample of ViewModel and LiveData

---
+++ Android/xfers_sdk/src/main/java/com/xfers/xfers_sdk/view_model/UserBankAccountsViewModel.kt
@@ -0,0 +1,25 @@
+package com.xfers.xfers_sdk.view_model
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.xfers.xfers_sdk.model.UserBankAccount
+
+class UserBankAccountsViewModel : ViewModel() {
+    private lateinit var userBankAccounts: MutableLiveData<List<UserBankAccount>>
+
+    fun getUserBankAccounts(): LiveData<List<UserBankAccount>> {
+        if (!::userBankAccounts.isInitialized) {
+            userBankAccounts = MutableLiveData()
+            loadUserBankAccounts()
+        }
+        return userBankAccounts
+    }
+
+    private fun loadUserBankAccounts() {
+        // TODO: Modify to an asynchronous operation to fetch userBankAccounts
+        val userBankAccountIpsum = UserBankAccount("DBS", "123-XXXXX-3")
+
+        userBankAccounts.postValue(listOf(userBankAccountIpsum, userBankAccountIpsum))
+    }
+}
