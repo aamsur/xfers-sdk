d52fa595a298e454f61a67aeba9087d269501c1a
Keong Loong Fuo <winskeong@gmail.com>
2018-11-04 18:14:22 +0800

    Save intermittantly

---
+++ Android/xfers_sdk/src/main/java/com/xfers/xfers_sdk/view/withdrawal/WithdrawalConfirmationActivity.kt
@@ -0,0 +1,61 @@
+package com.xfers.xfers_sdk.view.withdrawal
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.xfers.xfers_sdk.R
+import com.xfers.xfers_sdk.view.shared.TextRowItem
+import com.xfers.xfers_sdk.view.shared.XfersTextRowAdapter
+import kotlinx.android.synthetic.main.activity_withdrawal_confirmation.*
+import kotlinx.android.synthetic.main.xfers_double_buttons.*
+import kotlinx.android.synthetic.main.xfers_list_view.*
+import kotlinx.android.synthetic.main.xfers_summary_emphasis.*
+
+class WithdrawalConfirmationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_withdrawal_confirmation)
+
+        title = getString(R.string.withdrawal_confirmation_title)
+
+        withdrawalConfirmationTitleTextView.text = getString(R.string.withdrawal_confirmation_page_title)
+
+        val textRowItems = listOf(
+                TextRowItem(
+                        getString(R.string.withdrawal_confirmation_withdraw_from_copy),
+                        getString(R.string.withdrawal_confirmation_withdraw_from_ipsum)
+                ),
+                TextRowItem(
+                        getString(R.string.withdrawal_confirmation_to_bank_account_copy),
+                        getString(R.string.withdrawal_confirmation_to_bank_account_ipsum)
+                ),
+                TextRowItem(
+                        getString(R.string.withdrawal_confirmation_amount_copy),
+                        getString(R.string.withdrawal_confirmation_amount_ipsum)
+                ),
+                TextRowItem(
+                        getString(R.string.withdrawal_confirmation_processing_type_copy),
+                        getString(R.string.withdrawal_confirmation_processing_type_ipsum)
+                )
+        )
+
+        listViewRecyclerView.layoutManager = LinearLayoutManager(this)
+        val adapter = XfersTextRowAdapter(textRowItems)
+        listViewRecyclerView.adapter = adapter
+
+        summaryEmphasisTopTextView.text = getString(R.string.withdrawal_cofnirmation_emphasis_title)
+        summaryEmphasisBottomTextView.text = getString(R.string.withdrawal_confirmation_number_ipsum)
+
+        xfersDoubleButtonsNegativeButton.text = getString(R.string.cancel_button_copy)
+        xfersDoubleButtonsNegativeButton.setOnClickListener {
+            finish()
+        }
+
+        xfersDoubleButtonsPositiveButton.text = getString(R.string.confirm_button_copy)
+        xfersDoubleButtonsPositiveButton.setOnClickListener {
+            // TODO: Make it navigate to the correct screen, said screen not built yet
+            finish()
+        }
+    }
+}
