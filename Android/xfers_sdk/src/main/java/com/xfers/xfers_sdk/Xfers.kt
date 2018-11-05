59b889ee72490e96302d700ff52c3b861a89c5cb
junhaoyap <lordyyap@gmail.com>
2018-11-05 16:09:13 +0800

    Minor changes to kyc document confirmation

--- Android/xfers_sdk/src/main/java/com/xfers/xfers_sdk/Xfers.kt
+++ Android/xfers_sdk/src/main/java/com/xfers/xfers_sdk/Xfers.kt
@@ -6,7 +6,7 @@ import com.xfers.xfers_sdk.utils.NetworkClient
 import com.xfers.xfers_sdk.utils.XfersConfiguration
 import com.xfers.xfers_sdk.view.shared.ComingSoonActivity
 import com.xfers.xfers_sdk.view.connect.ConnectPhoneActivity
-import com.xfers.xfers_sdk.view.kyc.KycMerchantEmailActivity
+import com.xfers.xfers_sdk.view.kyc.KycMotherMaidenNameActivity
 import com.xfers.xfers_sdk.view.topup.TopupBankSelectionActivity
 import com.xfers.xfers_sdk.view.pay.PaymentConfirmationActivity
 import com.xfers.xfers_sdk.view.manage_banks.add_bank_account.EnterNameActivity
@@ -54,7 +54,7 @@ class Xfers(val context: Context) {
 
         fun startKYCFlow() {
             XfersConfiguration.setMerchantFlowStartingContext(context)
-            context.startActivity(Intent(context, KycMerchantEmailActivity::class.java))
+            context.startActivity(Intent(context, KycMotherMaidenNameActivity::class.java))
         }
 
         fun startManageBanksFlow() {
