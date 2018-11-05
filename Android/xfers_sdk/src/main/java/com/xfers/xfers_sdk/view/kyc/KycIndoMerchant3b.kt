dad60f7ca12ea790f039a34953d4bc356da0ec62
donbader <qko589999@gmail.com>
2018-11-05 09:07:43 +0800

    Add page KycIndoMerchant3b

---
+++ Android/xfers_sdk/src/main/java/com/xfers/xfers_sdk/view/kyc/KycIndoMerchant3b.kt
@@ -0,0 +1,26 @@
+package com.xfers.xfers_sdk.view.kyc
+
+import android.os.Bundle
+import android.text.InputType
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.xfers.xfers_sdk.R
+import kotlinx.android.synthetic.main.xfers_form_input.*
+
+class KycIndoMerchant3b : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mother_maiden_name)
+
+        title = getString(R.string.kyc_indo_merchant_verification_title)
+
+        xfersFormInputFieldTitle.text = getString(R.string.kyc_indo_merchant_3b_input_field_title)
+        xfersFormInputEditText.hint = getString(R.string.kyc_indo_merchant_3b_email_placeholder)
+        xfersFormInputEditText.inputType = InputType.TYPE_CLASS_TEXT
+        xfersFormInputPageTitle.visibility = View.GONE
+
+        xfersFormInputEditTextSubtitle.text = getString(R.string.kyc_indo_merchant_3b_hint)
+        xfersFormInputNotesTextView.visibility = View.GONE
+    }
+}
