2faefa40ca83e92bb15e97f9308ab55771bc9ec3
junhaoyap <lordyyap@gmail.com>
2018-11-07 17:09:52 +0800

    Make connect identity verification activity use shared cards util

--- Android/xfers_sdk/src/main/java/com/xfers/xfers_sdk/view/connect/ConnectIdentityVerificationActivity.kt
+++ Android/xfers_sdk/src/main/java/com/xfers/xfers_sdk/view/connect/ConnectIdentityVerificationActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.xfers.xfers_sdk.R
+import com.xfers.xfers_sdk.utils.XfersStatusCardService
 import com.xfers.xfers_sdk.view.shared.ItemRowItem
 import com.xfers.xfers_sdk.view.shared.XfersItemRowAdapter
 import kotlinx.android.synthetic.main.xfers_double_buttons.*
@@ -45,8 +46,8 @@ class ConnectIdentityVerificationActivity : AppCompatActivity() {
 
         xfersDoubleButtonsPositiveButton.text = getString(R.string.proceed_button_copy)
         xfersDoubleButtonsPositiveButton.setOnClickListener {
-            // TODO: Present KYC flow, said page not built yet, for now just finish()
-            finish()
+            // TODO: Present KYC flow, said page not built yet, for now just present coming soon
+            XfersStatusCardService(this).presentComingSoonStatusCard()
         }
     }
 }
