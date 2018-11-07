5d6008a084c24d53450464abbe614ba8c600010b
kaikj <bluesword246@gmail.com>
2018-11-07 11:08:08 +0800

    Data model for bank info

---
+++ Android/xfers_sdk/src/main/java/com/xfers/xfers_sdk/view_model/BanksViewModel.kt
@@ -0,0 +1,28 @@
+package com.xfers.xfers_sdk.view_model
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.xfers.xfers_sdk.model.Bank
+
+class BanksViewModel : ViewModel() {
+    private val banks = MutableLiveData<List<Bank>>()
+
+    fun getBanks(): LiveData<List<Bank>> {
+        loadBanks()
+        return banks
+    }
+
+    private fun loadBanks() {
+        // TODO: Modify to an asynchronous operation to fetch supported banks
+        val banksIpsum = Bank("Bank BCA")
+        banks.postValue(listOf(
+                Bank("Bank BCA"),
+                Bank("Bank Mandiri"),
+                Bank("Bank BRI"),
+                Bank("Bank BNI"),
+                Bank("Bank CIMB Niaga"),
+                Bank("Bank Permata")
+        ))
+    }
+}
