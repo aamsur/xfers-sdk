f7d96e768b235aeb4615719a2f6c3b4cff0795bc
junhaoyap <lordyyap@gmail.com>
2018-11-13 17:53:41 +0800

    Add kyc ktp activity and update document preparation

--- Android/xfers_sdk/src/main/java/com/xfers/xfers_sdk/view/kyc/KycKtpActivity.kt
+++ Android/xfers_sdk/src/main/java/com/xfers/xfers_sdk/view/kyc/KycKtpActivity.kt
@@ -8,24 +8,24 @@ import com.xfers.xfers_sdk.R
 import kotlinx.android.synthetic.main.xfers_button.*
 import kotlinx.android.synthetic.main.xfers_kyc_form_input.*
 
-class KycMotherMaidenNameActivity : AppCompatActivity() {
+class KycKtpActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_mother_maiden_name)
+        setContentView(R.layout.activity_kyc_ktp)
 
         title = getString(R.string.kyc_input_form_title)
 
         xfersKycFormInputStepTitle.text = getString(R.string.kyc_input_form_step_title)
-        xfersKycFormInputStepNumber.text = getString(R.string.kyc_mother_maiden_name_step_number)
-        xfersKycFormInputFieldTitle.text = getString(R.string.kyc_mother_maiden_name_page_title)
-        xfersKycFormInputEditText.hint = getString(R.string.kyc_mother_maiden_name_placeholder)
+        xfersKycFormInputStepNumber.text = getString(R.string.kyc_ktp_step_number)
+        xfersKycFormInputFieldTitle.text = getString(R.string.kyc_ktp_page_title)
+        xfersKycFormInputEditText.hint = getString(R.string.kyc_ktp_placeholder)
         xfersKycFormInputEditText.inputType = InputType.TYPE_CLASS_TEXT
 
         xfersFullWidthButton.setOnClickListener {
             // TODO: Push mother's maiden name information to next activity
 
-            startActivity(Intent(this, KycDocumentsConfirmationActivity::class.java))
+//            startActivity(Intent(this, KycDocumentsConfirmationActivity::class.java))
         }
     }
 }
