4cf22c1bb20d2b112df9537cfc1c5dfd100771ef
junhaoyap <lordyyap@gmail.com>
2018-11-13 18:00:00 +0800

    Add full name form

--- Android/xfers_sdk/src/main/java/com/xfers/xfers_sdk/view/kyc/KycFullNameActivity.kt
+++ Android/xfers_sdk/src/main/java/com/xfers/xfers_sdk/view/kyc/KycFullNameActivity.kt
@@ -8,24 +8,24 @@ import com.xfers.xfers_sdk.R
 import kotlinx.android.synthetic.main.xfers_button.*
 import kotlinx.android.synthetic.main.xfers_kyc_form_input.*
 
-class KycKtpActivity : AppCompatActivity() {
+class KycFullNameActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_kyc_ktp)
+        setContentView(R.layout.activity_kyc_full_name)
 
         title = getString(R.string.kyc_input_form_title)
 
         xfersKycFormInputStepTitle.text = getString(R.string.kyc_input_form_step_title)
-        xfersKycFormInputStepNumber.text = getString(R.string.kyc_ktp_step_number)
-        xfersKycFormInputFieldTitle.text = getString(R.string.kyc_ktp_page_title)
-        xfersKycFormInputEditText.hint = getString(R.string.kyc_ktp_placeholder)
+        xfersKycFormInputStepNumber.text = getString(R.string.kyc_full_name_step_number)
+        xfersKycFormInputFieldTitle.text = getString(R.string.kyc_full_name_page_title)
+        xfersKycFormInputEditText.hint = getString(R.string.kyc_full_name_placeholder)
         xfersKycFormInputEditText.inputType = InputType.TYPE_CLASS_TEXT
 
         xfersFullWidthButton.setOnClickListener {
-            // TODO: Push KTP information to following activities
+            // TODO: Push full name information to following activities
 
-            startActivity(Intent(this, KycFullNameActivity::class.java))
+//            startActivity(Intent(this, KycDocumentsConfirmationActivity::class.java))
         }
     }
 }
