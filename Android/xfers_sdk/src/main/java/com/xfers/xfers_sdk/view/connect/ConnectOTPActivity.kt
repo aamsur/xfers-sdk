a1d804363c5a40ade9a0d63344dfae8348be5fa9
junhaoyap <lordyyap@gmail.com>
2018-11-15 17:12:29 +0800

    Make resend OTP coming soon

--- Android/xfers_sdk/src/main/java/com/xfers/xfers_sdk/view/connect/ConnectOTPActivity.kt
+++ Android/xfers_sdk/src/main/java/com/xfers/xfers_sdk/view/connect/ConnectOTPActivity.kt
@@ -4,10 +4,12 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.xfers.xfers_sdk.R
 import com.xfers.xfers_sdk.view_model.ConnectOTPViewModel
+import kotlinx.android.synthetic.main.activity_connect_otp.*
 import kotlinx.android.synthetic.main.xfers_button.*
 
 class ConnectOTPActivity : AppCompatActivity() {
@@ -35,6 +37,9 @@ class ConnectOTPActivity : AppCompatActivity() {
             connectOTPViewModel.connectOTP(OTP).observe(this, connectSuccessObserver)
         }
 
-        // TODO: Make Resend OTP text view clickable and actually resend OTP
+        connectOTPResentOTPTextView.setOnClickListener {
+            // TODO: Make Resend OTP text view actually resend OTP
+            Toast.makeText(this, "Resend OTP feature coming soon", Toast.LENGTH_SHORT).show()
+        }
     }
 }
