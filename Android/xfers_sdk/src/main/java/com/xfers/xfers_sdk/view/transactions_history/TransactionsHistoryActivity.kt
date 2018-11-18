c21feea7f7810b7afb6f8d78e99190f1dcfaa63a
wandersen <pangtongwushu@gmail.com>
2018-11-18 16:18:44 +0700

    can_hit_server_and_build_forever_loading_button

--- Android/xfers_sdk/src/main/java/com/xfers/xfers_sdk/view/transactions_history/TransactionsHistoryActivity.kt
+++ Android/xfers_sdk/src/main/java/com/xfers/xfers_sdk/view/transactions_history/TransactionsHistoryActivity.kt
@@ -2,16 +2,20 @@ package com.xfers.xfers_sdk.view.transactions_history
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.core.text.bold
 import androidx.core.text.buildSpannedString
 import androidx.core.text.color
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.xfers.xfers_sdk.R
-import com.xfers.xfers_sdk.utils.config.XfersConfiguration
+import com.xfers.xfers_sdk.model.UserActivity
 import com.xfers.xfers_sdk.view.shared.TransactionRowItem
 import com.xfers.xfers_sdk.view.shared.XfersTransactionRowAdapter
+import com.xfers.xfers_sdk.view_model.TransactionsHistoryViewModel
 import kotlinx.android.synthetic.main.activity_transactions_history.*
 import kotlinx.android.synthetic.main.xfers_list_view.*
 
@@ -29,292 +33,51 @@ class TransactionsHistoryActivity : AppCompatActivity() {
 
         // TODO: Display something cute when there isn't any transaction at all?
 
-        // TODO: Integrate viewModel and appropriate APIs
-        val transactionRowItems = listOf(
-                TransactionRowItem(
-                        R.drawable.withdraw_arrow_18, R.color.pastelOrange,
-                        buildSpannedString {
-                            bold {
-                                append(getString(R.string.transaction_history_withdraw_title))
-                            }
-                            append("\n")
-                            append(getString(R.string.transaction_history_to_bank_ipsum))
-                        },
-                        buildSpannedString {
-                            color(ContextCompat.getColor(context, R.color.pastelOrange)) {
-                                append(getString(R.string.transaction_history_negative_money_ipsum_2))
-                            }
-                            append("\n")
-                            append(getString(R.string.transaction_history_date_ipsum))
-                        }
-                ) {
-                    // TODO: Pass activity information through extras into child activity
-                    startActivity(Intent(this, TransactionHistoryActivity::class.java))
-                },
-                TransactionRowItem(
-                        R.drawable.deposit_arrow_18, R.color.aquaMarine,
-                        buildSpannedString {
-                            bold {
-                                append(getString(R.string.transaction_history_topup_request_title))
-                            }
-                            append("\n")
-                            append(getString(R.string.transaction_history_from_bank_ipsum))
-                        },
-                        buildSpannedString {
-                            color(ContextCompat.getColor(context, R.color.aquaMarine)) {
-                                append(getString(R.string.transaction_history_positive_money_ipsum))
-                            }
-                            append("\n")
-                            append(getString(R.string.transaction_history_date_ipsum))
-                        }
-                ) {
-                    // TODO: Pass activity information through extras into child activity
-                    startActivity(Intent(this, TransactionHistoryActivity::class.java))
-                },
-                TransactionRowItem(
-                        R.drawable.withdraw_arrow_18, R.color.pastelOrange,
-                        buildSpannedString {
-                            bold {
-                                append(getString(R.string.transaction_history_withdraw_title))
-                            }
-                            append("\n")
-                            append(getString(R.string.transaction_history_from_merchant_ipsum, XfersConfiguration.getMerchantName()))
-                        },
-                        buildSpannedString {
-                            color(ContextCompat.getColor(context, R.color.pastelOrange)) {
-                                append(getString(R.string.transaction_history_negative_money_ipsum_1))
-                            }
-                            append("\n")
-                            append(getString(R.string.transaction_history_date_ipsum))
-                        }
-                ) {
-                    // TODO: Pass activity information through extras into child activity
-                    startActivity(Intent(this, TransactionHistoryActivity::class.java))
-                },
-                TransactionRowItem(
-                        R.drawable.withdraw_arrow_18, R.color.pastelOrange,
-                        buildSpannedString {
-                            bold {
-                                append(getString(R.string.transaction_history_withdraw_title))
-                            }
-                            append("\n")
-                            append(getString(R.string.transaction_history_to_bank_ipsum))
-                        },
-                        buildSpannedString {
-                            color(ContextCompat.getColor(context, R.color.pastelOrange)) {
-                                append(getString(R.string.transaction_history_negative_money_ipsum_2))
-                            }
-                            append("\n")
-                            append(getString(R.string.transaction_history_date_ipsum))
-                        }
-                ) {
-                    // TODO: Pass activity information through extras into child activity
-                    startActivity(Intent(this, TransactionHistoryActivity::class.java))
-                },
-                TransactionRowItem(
-                        R.drawable.deposit_arrow_18, R.color.aquaMarine,
-                        buildSpannedString {
-                            bold {
-                                append(getString(R.string.transaction_history_topup_title))
-                            }
-                            append("\n")
-                            append(getString(R.string.transaction_history_from_bank_ipsum))
-                        },
-                        buildSpannedString {
-                            color(ContextCompat.getColor(context, R.color.aquaMarine)) {
-                                append(getString(R.string.transaction_history_positive_money_ipsum))
-                            }
-                            append("\n")
-                            append(getString(R.string.transaction_history_date_ipsum))
-                        }
-                ) {
-                    // TODO: Pass activity information through extras into child activity
-                    startActivity(Intent(this, TransactionHistoryActivity::class.java))
-                },
-                TransactionRowItem(
-                        R.drawable.withdraw_arrow_18, R.color.pastelOrange,
-                        buildSpannedString {
-                            bold {
-                                append(getString(R.string.transaction_history_digital_purchase_title))
-                            }
-                            append("\n")
-                            append(getString(R.string.transaction_history_from_merchant_ipsum, XfersConfiguration.getMerchantName()))
-                        },
-                        buildSpannedString {
-                            color(ContextCompat.getColor(context, R.color.pastelOrange)) {
-                                append(getString(R.string.transaction_history_negative_money_ipsum_1))
-                            }
-                            append("\n")
-                            append(getString(R.string.transaction_history_date_ipsum))
-                        }
-                ) {
-                    // TODO: Pass activity information through extras into child activity
-                    startActivity(Intent(this, TransactionHistoryActivity::class.java))
-                },
-                TransactionRowItem(
-                        R.drawable.withdraw_arrow_18, R.color.pastelOrange,
-                        buildSpannedString {
-                            bold {
-                                append(getString(R.string.transaction_history_digital_purchase_title))
-                            }
-                            append("\n")
-                            append(getString(R.string.transaction_history_to_merchant_ipsum, XfersConfiguration.getMerchantName()))
-                        },
-                        buildSpannedString {
-                            color(ContextCompat.getColor(context, R.color.pastelOrange)) {
-                                append(getString(R.string.transaction_history_negative_money_ipsum_2))
-                            }
-                            append("\n")
-                            append(getString(R.string.transaction_history_date_ipsum))
-                        }
-                ) {
-                    // TODO: Pass activity information through extras into child activity
-                    startActivity(Intent(this, TransactionHistoryActivity::class.java))
-                },
-                TransactionRowItem(
-                        R.drawable.withdraw_arrow_18, R.color.pastelOrange,
-                        buildSpannedString {
-                            bold {
-                                append(getString(R.string.transaction_history_withdraw_title))
-                            }
-                            append("\n")
-                            append(getString(R.string.transaction_history_to_bank_ipsum))
-                        },
-                        buildSpannedString {
-                            color(ContextCompat.getColor(context, R.color.pastelOrange)) {
-                                append(getString(R.string.transaction_history_negative_money_ipsum_2))
-                            }
-                            append("\n")
-                            append(getString(R.string.transaction_history_date_ipsum))
-                        }
-                ) {
-                    // TODO: Pass activity information through extras into child activity
-                    startActivity(Intent(this, TransactionHistoryActivity::class.java))
-                },
-                TransactionRowItem(
-                        R.drawable.deposit_arrow_18, R.color.aquaMarine,
-                        buildSpannedString {
-                            bold {
-                                append(getString(R.string.transaction_history_topup_request_title))
-                            }
-                            append("\n")
-                            append(getString(R.string.transaction_history_from_bank_ipsum))
-                        },
-                        buildSpannedString {
-                            color(ContextCompat.getColor(context, R.color.aquaMarine)) {
-                                append(getString(R.string.transaction_history_positive_money_ipsum))
-                            }
-                            append("\n")
-                            append(getString(R.string.transaction_history_date_ipsum))
-                        }
-                ) {
-                    // TODO: Pass activity information through extras into child activity
-                    startActivity(Intent(this, TransactionHistoryActivity::class.java))
-                },
-                TransactionRowItem(
-                        R.drawable.withdraw_arrow_18, R.color.pastelOrange,
-                        buildSpannedString {
-                            bold {
-                                append(getString(R.string.transaction_history_withdraw_title))
-                            }
-                            append("\n")
-                            append(getString(R.string.transaction_history_from_merchant_ipsum, XfersConfiguration.getMerchantName()))
-                        },
-                        buildSpannedString {
-                            color(ContextCompat.getColor(context, R.color.pastelOrange)) {
-                                append(getString(R.string.transaction_history_negative_money_ipsum_1))
-                            }
-                            append("\n")
-                            append(getString(R.string.transaction_history_date_ipsum))
-                        }
-                ) {
-                    // TODO: Pass activity information through extras into child activity
-                    startActivity(Intent(this, TransactionHistoryActivity::class.java))
-                },
-                TransactionRowItem(
-                        R.drawable.withdraw_arrow_18, R.color.pastelOrange,
-                        buildSpannedString {
-                            bold {
-                                append(getString(R.string.transaction_history_withdraw_title))
-                            }
-                            append("\n")
-                            append(getString(R.string.transaction_history_to_bank_ipsum))
-                        },
-                        buildSpannedString {
-                            color(ContextCompat.getColor(context, R.color.pastelOrange)) {
-                                append(getString(R.string.transaction_history_negative_money_ipsum_2))
-                            }
-                            append("\n")
-                            append(getString(R.string.transaction_history_date_ipsum))
-                        }
-                ) {
-                    // TODO: Pass activity information through extras into child activity
-                    startActivity(Intent(this, TransactionHistoryActivity::class.java))
-                },
-                TransactionRowItem(
-                        R.drawable.deposit_arrow_18, R.color.aquaMarine,
-                        buildSpannedString {
-                            bold {
-                                append(getString(R.string.transaction_history_topup_title))
-                            }
-                            append("\n")
-                            append(getString(R.string.transaction_history_from_bank_ipsum))
-                        },
-                        buildSpannedString {
-                            color(ContextCompat.getColor(context, R.color.aquaMarine)) {
-                                append(getString(R.string.transaction_history_positive_money_ipsum))
-                            }
-                            append("\n")
-                            append(getString(R.string.transaction_history_date_ipsum))
-                        }
-                ) {
-                    // TODO: Pass activity information through extras into child activity
-                    startActivity(Intent(this, TransactionHistoryActivity::class.java))
-                },
-                TransactionRowItem(
-                        R.drawable.withdraw_arrow_18, R.color.pastelOrange,
-                        buildSpannedString {
-                            bold {
-                                append(getString(R.string.transaction_history_digital_purchase_title))
-                            }
-                            append("\n")
-                            append(getString(R.string.transaction_history_from_merchant_ipsum, XfersConfiguration.getMerchantName()))
-                        },
-                        buildSpannedString {
-                            color(ContextCompat.getColor(context, R.color.pastelOrange)) {
-                                append(getString(R.string.transaction_history_negative_money_ipsum_1))
-                            }
-                            append("\n")
-                            append(getString(R.string.transaction_history_date_ipsum))
-                        }
-                ) {
-                    // TODO: Pass activity information through extras into child activity
-                    startActivity(Intent(this, TransactionHistoryActivity::class.java))
-                },
-                TransactionRowItem(
-                        R.drawable.withdraw_arrow_18, R.color.pastelOrange,
-                        buildSpannedString {
-                            bold {
-                                append(getString(R.string.transaction_history_digital_purchase_title))
-                            }
-                            append("\n")
-                            append(getString(R.string.transaction_history_to_merchant_ipsum, XfersConfiguration.getMerchantName()))
-                        },
-                        buildSpannedString {
-                            color(ContextCompat.getColor(context, R.color.pastelOrange)) {
-                                append(getString(R.string.transaction_history_negative_money_ipsum_2))
-                            }
-                            append("\n")
-                            append(getString(R.string.transaction_history_date_ipsum))
-                        }
-                ) {
-                    // TODO: Pass activity information through extras into child activity
-                    startActivity(Intent(this, TransactionHistoryActivity::class.java))
-                }
-        )
-
-        listViewRecyclerView.layoutManager = LinearLayoutManager(this)
-        val adapter = XfersTransactionRowAdapter(this, transactionRowItems)
-        listViewRecyclerView.adapter = adapter
+        observeViewModel()
     }
+
+    override fun onResume() {
+        super.onResume()
+        observeViewModel()
+    }
+
+    private fun observeViewModel() {
+        transactionsHistoryProgressBar.visibility = View.VISIBLE
+        transactionsHistoryListView.visibility = View.GONE
+
+        val transactionHistoriesViewModel = ViewModelProviders.of(this).get(TransactionsHistoryViewModel::class.java)
+        transactionHistoriesViewModel.getTransactionHistories().observe(this, Observer<List<UserActivity>> {
+            transactionsHistoryProgressBar.visibility = View.GONE
+            transactionsHistoryListView.visibility = View.VISIBLE
+
+            val transactionRowItems = it.map { transactionHistory ->
+                TransactionRowItem(
+                    R.drawable.withdraw_arrow_18,
+                    R.color.pastelOrange,
+                    buildSpannedString {
+                        bold {
+                            append(transactionHistory.type)
+                        }
+                    },
+                    buildSpannedString {
+                        color(ContextCompat.getColor(this@TransactionsHistoryActivity, R.color.pastelOrange)) {
+                            append(transactionHistory.amount)
+                        }
+                        append("\n")
+                        append(transactionHistory.createdAt)
+                    },
+                    onClick = {
+                        // TODO: Pass activity information through extras into child activity
+                        startActivity(Intent(this, TransactionHistoryActivity::class.java))
+                    }
+                )
+            }
+
+            listViewRecyclerView.layoutManager = LinearLayoutManager(this)
+            val adapter = XfersTransactionRowAdapter(this, transactionRowItems)
+            listViewRecyclerView.adapter = adapter
+
+        })
+    }
+
 }
---
+++ Android/xfers_sdk/src/main/java/com/xfers/xfers_sdk/view_model/TransactionsHistoryViewModel.kt
@@ -0,0 +1,51 @@
+package com.xfers.xfers_sdk.view_model
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.xfers.xfers_sdk.model.UserActivity
+import com.xfers.xfers_sdk.utils.XfersRepository
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+
+class TransactionsHistoryViewModel : ViewModel() {
+    private val xfersRepository = XfersRepository()
+    private val transactionHistories = MutableLiveData<List<UserActivity>>()
+    private var subscription: Disposable? = null
+
+    fun getTransactionHistories(): LiveData<List<UserActivity>> {
+        subscription = xfersRepository.getActivities(100)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnSubscribe { onGetTransactionHistoriesStart() }
+                .doOnTerminate { onGetTransactionHistoriesFinish() }
+                .subscribe(
+                        { onGetTransactionHistoriesSuccess(it.activites!!) },
+                        { onGetTransactionHistoriesError() }
+                )
+
+        return transactionHistories
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        subscription?.dispose()
+    }
+
+    private fun onGetTransactionHistoriesStart() {
+        // TODO: Provide observable to show loading on view
+    }
+
+    private fun onGetTransactionHistoriesFinish() {
+        // TODO: Provide observable to show
+    }
+
+    private fun onGetTransactionHistoriesSuccess(UserActivitiesList: List<UserActivity>) {
+        transactionHistories.postValue(UserActivitiesList)
+    }
+
+    private fun onGetTransactionHistoriesError() {
+        // TODO: Provide observable to show error on view
+    }
+}
